aadcce2478f39e932fba93847f4e124c8c517369
Kplimzy <superartistic2911@gmail.com>
2025-07-23 20:01:45 +0000

    Add files via upload

---
+++ Diode_Signal_Detector.ino
@@ -0,0 +1,26 @@
+//Diode Signal Detector on ESP32
+
+const int signalPin = 25; // Pin connected after the diode
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(signalPin, INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int signalState = digitalRead(signalPin);
+
+  if (signalState == HIGH) {
+    Serial.println(" High voltage detected!");
+  } 
+  else{
+    Serial.println("No high voltage detected (safe).");
+  }
+
+  delay(500);
+  // put your main code here, to run repeatedly:
+
+}
